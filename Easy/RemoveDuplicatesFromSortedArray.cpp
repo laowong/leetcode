092a6e0e28aa0d0b13f134ca824b5292570196f2
newbie1993 <newbie1993.king@gmail.com>
2017-08-22 23:02:00 +0800

    Remove Duplicates from Sorted Array

---
+++ Easy/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,66 @@
+/*
+ * 题目：Remove Duplicates from Sorted Array
+ * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ *
+ * For example,
+ * Given input array nums = [1,1,2],
+ *
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+ *
+ * 链接：https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ *
+ */
+ 
+/*
+ * 我最初的解法：(29ms)
+ */
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        if (0 == size || 1 == size)
+        {
+            return size;
+        }
+        int newLength = 1, preValue = nums[0], pos = 1;
+        for (int i = 1; i < size; ++i)
+        {
+            if (preValue != nums[i])
+            {
+                preValue = nums[i];
+                nums[pos++] = nums[i];
+                ++newLength;
+            }
+        }
+        return newLength;
+    }
+};
+
+/*
+ * 优化后的解法：(25ms)
+ */
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        if (0 == size || 1 == size)
+        {
+            return size;
+        }
+        int dupCount = 0;
+        for (int i = 1; i < size; ++i)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                ++dupCount;
+            }
+            else
+            {
+                nums[i - dupCount] = nums[i];
+            }
+        }
+        return size - dupCount;
+    }
+};
